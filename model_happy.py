048345ffd53e7d4d2b716a831f0f97dc58b11557
Yuvia Cardenas <yuvia.cardenas1@gmail.com>
2022-12-15 22:04:19 -0600

    modelfile

--- model_happy.py
+++ model_happy.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
 import os
-from sklearn.metrics import mean_squared_error
+
+from sklearn.metrics import mean_squared_error, r2_score, explained_variance_score
 from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import SelectKBest, RFE, f_regression, SequentialFeatureSelector
-from sklearn.linear_model import LassoLars
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import TweedieRegressor
 
@@ -35,29 +35,34 @@ def model_sets(train,validate,test):
     predicting variables (x) and target variable (y)
     ''' 
 
-    x_train = train.drop(columns=['Happiness_Score', 'Happiness_Rank'])
+    x_train = train.drop(columns=['Happiness_Score', 'Happiness_Rank', 'Country'])
     y_train = train.Happiness_Score
 
 
-    x_validate = validate.drop(columns=['Happiness_Score', 'Happiness_Rank'])
+    x_validate = validate.drop(columns=['Happiness_Score', 'Happiness_Rank','Country'])
     y_validate = validate.Happiness_Score
 
-    x_test = test.drop(columns=['Happiness_Score', 'Happiness_Rank'])
+    x_test = test.drop(columns=['Happiness_Score', 'Happiness_Rank', 'Country'])
     y_test = test.Happiness_Score
 
     return x_train, y_train, x_validate, y_validate, x_test, y_test
 
-def predictions(train, validate):
+def predict(train, validate, test):
     '''
     Function takes the baseline and creates dataframe
     '''
     train_predictions = pd.DataFrame({
     'Happiness_Score': train.Happiness_Score})
+    train_predictions["Baseline"]= train.Happiness_Score.mean()
     validate_predictions = pd.DataFrame({
     'Happiness_Score': validate.Happiness_Score})
-    return train_predictions, validate_predictions
+    validate_predictions["Baseline"]= train.Happiness_Score.mean()
+    test_predictions = pd.DataFrame({
+    'Happiness_Score': test.Happiness_Score})
+    
+    return train_predictions, validate_predictions, test_predictions
 
-def simple_lm_model(x_train, y_train, x_validate, train_predictions, validate_predictions):
+def simple_lm_model(train, x_train, y_train, validate, x_validate, train_predictions, validate_predictions):
     '''
     Function takes the predicted variable (x) and target variable (y)
     fits them to the simple linear regression model and computes the predictions 
@@ -65,31 +70,18 @@ def simple_lm_model(x_train, y_train, x_validate, train_predictions, validate_pr
     ''' 
     lm_model = LinearRegression().fit(x_train, y_train)
     train_predictions['lm_predictions'] = lm_model.predict(x_train)
+    train['lm_predictions'] = lm_model.predict(x_train)
     validate_predictions['lm_predictions'] = lm_model.predict(x_validate)
+    validate['lm_predictions'] = lm_model.predict(x_validate)
 
     lm_co = lm_model.coef_
     lm_int = lm_model.intercept_
 
     simp_co = pd.Series(lm_model.coef_, index=x_train.columns).sort_values()
     print(simp_co)
-    return train_predictions, validate_predictions
+    return train, train_predictions, validate, validate_predictions
 
-def lasso_model(x_train, y_train, x_validate, train_predictions, validate_predictions):
-    '''
-    Function takes the predicted variable (x) and target variable (y)
-    fits them to the lasso lars regression model and computes the predictions 
-    and then adds them to the dataframe  
-    ''' 
-
-    lars = LassoLars(alpha=1).fit(x_train, y_train)
-    train_predictions['lars_predictions'] = lars.predict(x_train)
-    validate_predictions['lars_predictions'] = lars.predict(x_validate)
-
-    lars_co = pd.Series(lars.coef_, index=x_train.columns).sort_values()
-    print(lars_co)
-    return train_predictions, validate_predictions
-
-def glm_model(x_train, y_train, x_validate, train_predictions, validate_predictions):
+def glm_model(train, x_train, y_train, validate, x_validate, train_predictions, validate_predictions):
     '''
     Function takes the predicted variable (x) and target variable (y)
     fits them to the generalized linear regression model and computes the predictions 
@@ -98,42 +90,36 @@ def glm_model(x_train, y_train, x_validate, train_predictions, validate_predicti
 
     glm_model = TweedieRegressor(power=0, alpha=1).fit(x_train, y_train)
     train_predictions['glm_predictions'] = glm_model.predict(x_train)
+    train['glm_predictions'] = glm_model.predict(x_train)
     validate_predictions['glm_predictions'] = glm_model.predict(x_validate)
+    validate['glm_predictions'] = glm_model.predict(x_validate)
 
     glm_co = pd.Series(glm_model.coef_, index=x_train.columns).sort_values()
     print(glm_co)
-    return train_predictions, validate_predictions
+    return train, train_predictions, validate, validate_predictions
 
 ###################################################
 ################## Evaluate Models on Train #######
 ###################################################
 
 def baseline_mean_errors(train):
-    y = train.Happiness_Score
-    baseline = train.Happiness_Score.mean()
     
-    MSE = mean_squared_error(y, baseline)
-    SSE = MSE * len(y)
-    RMSE = MSE**.5
-    
-    print("SSE:", round(SSE, 2),"\nMSE: ", round(MSE, 2), 
-      "\nRMSE: ", round(RMSE, 2))
-
-def lm_errors(train):
+    train['Baseline']= train.Happiness_Score.mean()
     y = train.Happiness_Score
-    yhat = train.lm_predictions
-
+    yhat = train.Baseline
+    
     MSE = mean_squared_error(y, yhat)
     SSE = MSE * len(y)
     RMSE = MSE**.5
     
     print("SSE:", round(SSE, 2),"\nMSE: ", round(MSE, 2), 
       "\nRMSE: ", round(RMSE, 2))
-    
 
-def lars_errors(train):
+    
+def lm_errors(train):
     y = train.Happiness_Score
-    yhat = train.lars_predictions
+    yhat = train.lm_predictions
+
     MSE = mean_squared_error(y, yhat)
     SSE = MSE * len(y)
     RMSE = MSE**.5
@@ -151,3 +137,56 @@ def glm_errors(train):
     
     print("SSE:", round(SSE, 2),"\nMSE: ", round(MSE, 2), 
       "\nRMSE: ", round(RMSE, 2))
+
+def test_lm_model(test, x_test, y_test, validate, x_validate, test_predictions, validate_predictions):
+    '''
+    Function takes the predicted variable (x) and target variable (y)
+    fits them to the simple linear regression model and computes the predictions 
+    and then adds them to the dataframe  
+    ''' 
+    lm_model = LinearRegression().fit(x_test, y_test)
+    test_predictions['lm_predictions'] = lm_model.predict(x_test)
+    test['lm_predictions'] = lm_model.predict(x_test)
+    
+    lm_co = lm_model.coef_
+    lm_int = lm_model.intercept_
+
+    simp_co = pd.Series(lm_model.coef_, index=x_test.columns).sort_values()
+    print(simp_co)
+    return test, test_predictions, validate, validate_predictions
+
+def lm_test_errors(test):
+    y = test.Happiness_Score
+    yhat = test.lm_predictions
+
+    MSE = mean_squared_error(y, yhat)
+    SSE = MSE * len(y)
+    RMSE = MSE**.5
+    
+    print("SSE:", round(SSE, 2),"\nMSE: ", round(MSE, 2), 
+      "\nRMSE: ", round(RMSE, 2))
+
+def make_metric_df(y, y_hat, model_name, metric_df):
+    if metric_df.size ==0:
+        metric_df = pd.DataFrame(data=[
+            {
+                'model': model_name, 
+                'RMSE': mean_squared_error(
+                    y,
+                    y_hat) ** .5,
+                'r^2': explained_variance_score(
+                    y,
+                    y_hat)
+            }])
+        return metric_df
+    else:
+        return metric_df.append(
+            {
+                'model': model_name, 
+                'RMSE': mean_squared_error(
+                    y,
+                    y_hat) ** .5,
+                'r^2': explained_variance_score(
+                    y,
+                    y_hat)
+            }, ignore_index=True)
